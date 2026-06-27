781c50d3fc2611e935b0685789eee63e83606f02
Will Toohey <will@mon.im>
2026-06-27 17:46:57 +1000

    Remove move macro usages from the logger

--- src/log.hpp
+++ src/log.hpp
@@ -13,10 +13,13 @@ inline constexpr char _log_module[] = LOG_MODULE;
 
 using log_formatter_t = void (*)(const char *module, const char *fmt, ...);
 
-template<log_formatter_t* Logger, const char *Module, class... Args>
-void log_base(fmt::format_string<Args...> fmt, Args&&... args) {
-    (*Logger)(Module, "%s", fmt::format(fmt, std::forward<Args>(args)...).c_str());
-}
+template<log_formatter_t* Logger, const char *Module>
+struct log_base {
+    template<class... Args>
+    void operator()(fmt::format_string<Args...> fmt, Args&&... args) const {
+        (*Logger)(Module, "%s", fmt::format(fmt, std::forward<Args>(args)...).c_str());
+    }
+};
 
 
 // functions that default to file output, but will be overriden to point to AVS
@@ -26,10 +29,10 @@ extern log_formatter_t imp_log_body_warning;
 extern log_formatter_t imp_log_body_info;
 extern log_formatter_t imp_log_body_misc;
 
-#define log_fatal(...) log_base<&imp_log_body_fatal, _log_module>(__VA_ARGS__);
-#define log_warning(...) log_base<&imp_log_body_warning, _log_module>(__VA_ARGS__);
-#define log_info(...) log_base<&imp_log_body_info, _log_module>(__VA_ARGS__);
-#define log_misc(...) log_base<&imp_log_body_misc, _log_module>(__VA_ARGS__);
+inline constexpr log_base<&imp_log_body_fatal, _log_module> log_fatal;
+inline constexpr log_base<&imp_log_body_warning, _log_module> log_warning;
+inline constexpr log_base<&imp_log_body_info, _log_module> log_info;
+inline constexpr log_base<&imp_log_body_misc, _log_module> log_misc;
 // layeredfs super-verbose (since most people have loglevel misc already)
 #define log_verbose(...) if(config.verbose_logs) {log_misc(__VA_ARGS__);}
 
